4f39259c65f40e79cc8d01d25596a7df6c8fca15
winston-lim <winston_lim1@hotmail.com>
2021-03-11 22:02:36 +0800

    Update user schema

--- src/models/user.js
+++ src/models/user.js
@@ -1,43 +1,72 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
-const User = mongoose.model('User', {
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  email: {
-		type: String,
-		required: true,
-    trim: true,
-    lowercase: true,
-		validate(value) {
-			if (!validator.isEmail(value)) {
-        throw new Error('Invalid email');
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+      lowercase: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error('Invalid email');
+        }
       }
-    }
-	},
-  password: {
-    type: String,
-    required: true,
-    trim: true,
-    minlength: 7,
-    validate(value) {
-      if(value.toLowerCase().includes('password')) {
-        throw new Error('Password contains \'password\'');
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 7,
+      validate(value) {
+        if(value.toLowerCase().includes('password')) {
+          throw new Error('Password contains \'password\'');
+        }
       }
-    }
-  },
-  age: {
-    type: Number,
-    default: 0,
-    validate(value) {
-      if(value<0) {
-        throw new Error('Invalid age');
+    },
+    age: {
+      type: Number,
+      default: 0,
+      validate(value) {
+        if(value<0) {
+          throw new Error('Invalid age');
+        }
       }
     }
   }
+);
+
+userSchema.statics.login = async(email,password) => {
+	const user  = await User.findOne({email}) //same as {email: email}
+	if (!user) {
+    console.log('User does not exist')
+		throw new Error('Unable to login');
+	}
+
+	const isMatch = await bcrypt.compare(password, user.password);
+	if (!isMatch) {
+    console.log('Not matching')
+		throw new Error('Incorrent login credentials');
+	}
+	return user;
+}
+
+userSchema.pre('save', async function(next) {
+  const user = this;
+  if (user.isModified('password')) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
+  next();
 })
+
+const User = mongoose.model('User', userSchema)
 
 module.exports = User;
