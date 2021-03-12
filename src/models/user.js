dfcc40b3853deb44c449993e95a9e478b6e687c8
winston-lim <winston_lim1@hotmail.com>
2021-03-12 14:45:47 +0800

    Build generateToken static method

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const Task = require('./task');
 
 const userSchema = mongoose.Schema(
   {
@@ -40,9 +42,34 @@ const userSchema = mongoose.Schema(
           throw new Error('Invalid age');
         }
       }
-    }
+    },
+    tokens: [{
+      token: {
+        type: String,
+        required: true,
+      }
+    }]
+  },
+  {
+    timestamps: true,
   }
 );
+
+userSchema.methods.generateAuthToken = async function() {
+  const user = this;
+  const token = jwt.sign({_id: user._id.toString()}, 'privateKey');
+  user.tokens = user.tokens.concat({token});
+  await user.save();
+  return token;
+}
+
+userSchema.methods.toJSON = function() {
+  const user = this;
+  const userObject = user.toObject();
+  delete userObject.password;
+  delete userObject.tokens;  
+  return userObject;
+}
 
 userSchema.statics.login = async(email,password) => {
 	const user  = await User.findOne({email}) //same as {email: email}
@@ -64,6 +91,12 @@ userSchema.pre('save', async function(next) {
   if (user.isModified('password')) {
     user.password = await bcrypt.hash(user.password, 8);
   }
+  next();
+})
+
+userSchema.pre('remove',async function(next) {
+  const user = this;
+  await Task.deleteMany({owner: user._id});
   next();
 })
 
