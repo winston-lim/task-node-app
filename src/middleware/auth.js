c0b6cdd987f55a0e0ba2c11f3163f19c8e7902b1
winston-lim <winston_lim1@hotmail.com>
2021-03-12 14:44:43 +0800

    Build auth middleware

---
+++ src/middleware/auth.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+const auth = async (request,response,next) => {
+  try {
+    const token = request.header('Authorization').replace('Bearer ', '');
+    //decoded is the payload of the token if signature is correct
+    const decoded = jwt.verify(token, 'privateKey');
+    //_id of token is set to be same as _id of user on creation
+    //'tokens.token' string property syntax is for searching through the tokens array and comparing each token object to a value
+    const user = await User.findOne({_id: decoded._id, 'tokens.token': token});
+    if (!user) {
+      throw new Error();
+    }
+    //Access to current token is to be added to requests so that on logout, only current token is deleted
+    request.token = token;
+    //Simply return user so following middleware functions do not waste resources
+    request.user = user;
+    next();
+  } catch (e) {
+    response.status(401).send({error: 'Please authenticate'});
+  }
+}
+
+module.exports = auth;
