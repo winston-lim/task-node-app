a523210319f370ec730d2d097a32908fc2846e3c
winston-lim <winston_lim1@hotmail.com>
2021-03-13 23:58:07 +0800

    Refactor for testing

--- src/index.js
+++ src/index.js
@@ -1,17 +1,5 @@
-const express = require('express');
-//without assignment satatement, dependency is executed
-require('./db/mongoose');
-const userRouter = require('./routers/user');
-const taskRouter = require('./routers/task');
-const jwt = require('jsonwebtoken');
-
-
-const app = express();
+const app = require('./app');
 const port = process.env.PORT;
-
-app.use(express.json());
-app.use(userRouter);
-app.use(taskRouter);
 
 app.listen(port, ()=> {
   console.log('Server is up on port ', port); 
